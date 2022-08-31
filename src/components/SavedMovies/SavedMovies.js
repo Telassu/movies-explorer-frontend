9a9043a2de877dd624e38d8ecf61939db7fbaa77
Nadezhda <90825467+Telassu@users.noreply.github.com>
2022-08-31 23:21:36 +0300

    после КР_4

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -25,6 +25,7 @@ function SavedMovies({
   useEffect(() => {
     setIsNotMovies(false)
     setIsChecked(false)
+    setShownMovies(movies)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
